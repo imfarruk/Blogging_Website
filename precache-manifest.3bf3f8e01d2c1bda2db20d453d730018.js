b49909f4037a8aa3dc988a1e8d79dc86d0a74638
Md Farhan Haider <imfarruk@gmail.com>
2022-06-06 16:55:08 +0530

    Updates

---
+++ precache-manifest.3bf3f8e01d2c1bda2db20d453d730018.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "05c1aaaba19a8afb40a1",
+    "url": "/Blogging_Website/static/css/main.5e05e18b.chunk.css"
+  },
+  {
+    "revision": "05c1aaaba19a8afb40a1",
+    "url": "/Blogging_Website/static/js/main.05c1aaab.chunk.js"
+  },
+  {
+    "revision": "b55f4b777fcf29d43ef7",
+    "url": "/Blogging_Website/static/js/1.b55f4b77.chunk.js"
+  },
+  {
+    "revision": "0e8bbf23a70bb58025c5",
+    "url": "/Blogging_Website/static/css/2.036b964c.chunk.css"
+  },
+  {
+    "revision": "0e8bbf23a70bb58025c5",
+    "url": "/Blogging_Website/static/js/2.0e8bbf23.chunk.js"
+  },
+  {
+    "revision": "dd8154d1861a2d5c288d",
+    "url": "/Blogging_Website/static/js/runtime~main.dd8154d1.js"
+  },
+  {
+    "revision": "d6fa47619f822bc65ff5e299126c2995",
+    "url": "/Blogging_Website/index.html"
+  }
+];
